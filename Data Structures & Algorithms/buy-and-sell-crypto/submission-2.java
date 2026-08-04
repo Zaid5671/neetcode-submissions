4ad57840e7ef1c55880cb58e3a0945c23732f269
Zaid Ahmed Khan <zaid.khan24@spit.ac.in>
2026-08-04 19:58:07 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] nums) {
+        int l = 0;
+        int r = nums.length-1;
+
+        int min = nums[0];
+        int maxProfit = 0;
+
+        for(int i = 1;i<nums.length;i++){
+            maxProfit = Math.max(maxProfit,nums[i]-min);
+            if(nums[i]<min){
+                min = nums[i];
+            }
+        }
+        return maxProfit;    
+        }
+}
